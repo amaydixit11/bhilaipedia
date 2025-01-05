4d3ea28f76d7154fe989f2dae4c6c64e0db87a04
amaydixit11 <amaydixit11@gmail.com>
2025-01-06 02:28:51 +0530

    backend setup

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './modules/auth/auth.module';
+import { UserModule } from './modules/user/user.module';
+import { ArticleModule } from './modules/article/article.module';
+import { DiscussionModule } from './modules/discussion/discussion.module';
+import { ModerationModule } from './modules/moderation/moderation.module';
+import { RewardsModule } from './modules/rewards/rewards.module';
+import { SearchModule } from './modules/search/search.module';
+import { CommonModule } from './common/common.module';
+import { DatabaseModule } from './modules/database/database.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UserModule, ArticleModule, DiscussionModule, ModerationModule, RewardsModule, SearchModule, CommonModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
